d688c2841182a4d376fc7d0184bd6d8eb7eb69ab
Michael Buchar <michaelbuchar0@gmail.com>
2021-11-24 21:56:07 -0500

    Vue dynamic components testing

--- EventRegistration-Frontend/src/components/EventRegistration.vue
+++ EventRegistration-Frontend/src/components/EventRegistration.vue
@@ -3,26 +3,37 @@
     <h2>People</h2>
     <table>
       <tr>
-        <td>John</td>
+        <td>Person</td>
         <td>Event to attend</td>
+      </tr>
+      <tr v-for="person in people" :key="person.name" >
+        <td>{{ person.name }}</td>
+        <td>
+          <ul>
+            <li v-for="event in person.events" :key="event.name">
+              {{event.name}}
+            </li>
+          </ul>
+        </td>
       </tr>
       <tr>
         <td>
-          <input type="text" placeholder="Person Name">
+          <input type="text" v-model="newPerson" placeholder="Person Name">
         </td>
         <td>
-          <button>Create</button>
+          <button v-bind:disabled="!newPerson" @click="createPerson(newPerson)">Create Person</button>
         </td>
       </tr>
     </table>
     <p>
-      <span style="color:red">Error: Message text comes here</span>
+      <span v-if="errorPerson" style="color:red">Error: {{errorPerson}} </span>
     </p>
   </div>
 </template>
-<script>
 
+<script src="./registration.js">
 </script>
+
 <style>
   #eventregistration {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
